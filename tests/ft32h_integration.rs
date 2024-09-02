430aab13df416631b71022e7bd0c2217168659e0
Jan Berktold <jan@berktold.co>
2024-09-01 22:56:37 -0700

    iterate

--- tests/ft32h_integration.rs
+++ tests/ft32h_integration.rs
@@ -23,3 +23,37 @@ fn test_status() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn test_writing_to_file() -> anyhow::Result<()> {
+    let device = Ftdi::new()?;
+    let device: Ft232h = device.try_into()?;
+
+    let hal = FtHal::init_freq(device, 50_000)?;
+    let i2c = hal.i2c()?;
+
+    let mut logger = OpenLogger::new_and_validate(DeviceAddr::ADDR1, i2c)?;
+
+    let file = "test.txt";
+
+    // TODO: Delete file;
+
+    let size = logger.size(&file)?;
+    assert_eq!(size, 0);
+
+    println!("Creating file");
+
+    // Open the file for writing.
+    logger.append(&file)?;
+
+    println!("Writing to file");
+    logger.write_string("hello")?;
+
+    println!("Writing to file2");
+    logger.sync_file()?;
+
+    let size = logger.size(&file)?;
+    assert_eq!(size, 4);
+
+    Ok(())
+}
